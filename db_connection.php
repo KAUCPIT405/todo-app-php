e575b980dfcec2333e6659168742b360905a523d
Khalid Alharbi <kalharbi@users.noreply.github.com>
2017-12-15 20:24:40 +0300

    init commit

---
+++ db_connection.php
@@ -0,0 +1,19 @@
+<?php
+
+// WARNING: NEVER DO THIS IN PRODUCTION
+$servername = "localhost";
+$username = "kalharbi";
+$password = "";
+$database = "todo_db";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+echo $conn->connect_error;
+// check the connection
+if ($conn->connect_error) {
+    // exit and kill this process
+    echo "Failed to connect to database!";
+    die("Connection failed: ");
+}
+
+?>
